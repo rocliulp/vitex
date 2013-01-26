46a9088426c892c8aa74b731f7e085575c0fcf67
rocliulp <email@example.com>
2013-01-27 01:12:32 +0800

    Appended some classes for further usage.

---
+++ src/org/pl/BinSearcher.java
@@ -0,0 +1,8 @@
+package org.pl;
+
+import org.pl.Object;
+import org.pl.IBinSearcher;
+
+public class BinSearcher extends Object implements IBinSearcher {
+  // TODO:
+}
---
+++ src/org/pl/UnarySearcher.java
@@ -0,0 +1,8 @@
+package org.pl;
+
+import org.pl.Object;
+import org.pl.IUnarySearcher;
+
+public class UnarySearcher extends Object implements IUnarySearcher {
+  // TODO:
+}
